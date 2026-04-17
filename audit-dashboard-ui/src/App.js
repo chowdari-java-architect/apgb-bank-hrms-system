746a1c68924ce062b0d27270d7e4ea8532c4263d
ch chowdari <chau258@gmail.com>
2026-04-18 00:07:11 +0530

    Initial Bank HRMS backend, MySQL integration, employee dashboard UI, transfer workflow, audit tracker, with react front end ui

---
+++ audit-dashboard-ui/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AuditDashboard from "./components/AuditDashboard";
+
+function App() {
+  return (
+      <div>
+        <AuditDashboard />
+      </div>
+  );
+}
+
+export default App;
